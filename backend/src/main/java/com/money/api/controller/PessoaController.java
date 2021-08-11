8e690c9b6504e9b4ef14db04557e12a8c6fa8472
sumaeta <brenosumaeta33@gmail.com>
2021-08-11 10:00:26 -0300

    Implementando os metodos get e post

---
+++ backend/src/main/java/com/money/api/controller/PessoaController.java
@@ -0,0 +1,42 @@
+package com.money.api.controller;
+
+import com.money.api.model.Pessoa;
+import com.money.api.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @PostMapping
+    public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+        Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(pessoaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(pessoaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Pessoa> buscar(@PathVariable Long codigo){
+        Optional<Pessoa> categoria = pessoaRepository.findById(codigo);
+        if (categoria.isPresent()){
+            return ResponseEntity.ok().body(categoria.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
